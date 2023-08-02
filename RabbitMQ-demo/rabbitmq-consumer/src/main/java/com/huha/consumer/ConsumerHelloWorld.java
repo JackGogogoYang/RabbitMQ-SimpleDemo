7d789fd10c9be91573135257c0d09277800738cc
yhuan <yhuan@epoint.com.cn>
2023-08-02 10:35:12 +0800

    修改中文符号为英文

--- RabbitMQ-demo/rabbitmq-consumer/src/main/java/com/huha/consumer/ConsumerHelloWorld.java
+++ RabbitMQ-demo/rabbitmq-consumer/src/main/java/com/huha/consumer/ConsumerHelloWorld.java
@@ -64,11 +64,11 @@ public class ConsumerHelloWorld {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                 System.out.println("=============================================");
-                System.out.println("consumerTag：" + consumerTag);
-                System.out.println("Exchange：" + envelope.getExchange());
-                System.out.println("RoutingKey：" + envelope.getRoutingKey());
-                System.out.println("properties：" + properties);
-                System.out.println("body：" + new String(body));
+                System.out.println("consumerTag:" + consumerTag);
+                System.out.println("Exchange:" + envelope.getExchange());
+                System.out.println("RoutingKey:" + envelope.getRoutingKey());
+                System.out.println("properties:" + properties);
+                System.out.println("body:" + new String(body));
             }
         };
         channel.basicConsume(queueName, true, consumer);
